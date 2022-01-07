ba83fb74f835d1ae5059d7f7036cc1f41cc865f4
csy23333 <693765678@qq.com>
2022-01-07 18:01:07 +0800

    注册

--- api/user.go
+++ api/user.go
@@ -3,6 +3,7 @@ package api
 import (
 	"blog1222-go/mysql"
 	"fmt"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,7 @@ type user struct {
 	Password string `json:"password" db:"password"`
 }
 
+// 登录
 func Login(c *gin.Context) {
 	var userG user
 	if err := c.ShouldBindJSON(&userG); err != nil {
@@ -45,13 +47,15 @@ func Login(c *gin.Context) {
 }
 
 type registerS struct {
-	Acc      string `json:"account" db:"account"`   // 账号
-	Pass     string `json:"password" db:"password"` // 密码
-	UserName string `json:"username" db:"username"` // 用户名
-	Avatar   string `json:"avatar" db:"avatar"`     // 图像
-	Email    string `json:"email" db:"email"`       // 邮箱
+	Acc      string `json:"account"`   // 账号
+	Pass     string `json:"password"`  // 密码
+	Pass2    string `json:"password2"` // 重复密码
+	UserName string `json:"username" ` // 用户名
+	// Avatar   string `json:"avatar"`    // 头像
+	Email string `json:"email"` // 邮箱
 }
 
+// 注册
 func Register(c *gin.Context) {
 	var reg registerS
 
@@ -60,6 +64,35 @@ func Register(c *gin.Context) {
 		c.JSON(500, "serve error")
 		return
 	}
+	fmt.Println(reg)
+
+	// 表单校验
+	t := reflect.TypeOf(reg)
+	v := reflect.ValueOf(reg)
+
+	for k := 0; k < t.NumField(); k++ {
+		if v.Field(k).Interface() == "" {
+			c.JSON(200, gin.H{"code": "01", "message": "表单不完整"})
+			return
+		}
+	}
+
+	if reg.Pass != reg.Pass2 {
+		c.JSON(200, gin.H{"code": "01", "message": "密码不一致"})
+		return
+	}
+
+	// 入库
+	db := mysql.Db
+	sqlStr := "insert into users (account,password,username,email) values (?,?,?,?)"
+
+	_, err := db.Exec(sqlStr, reg.Acc, reg.Pass, reg.UserName, reg.Email)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.JSON(500, "bad")
+		return
+	}
+
 	fmt.Print(reg.Email)
 	c.JSON(200, "ok")
 }
